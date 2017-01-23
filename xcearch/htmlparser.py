5158c4780bddc3d99704ef41783c11a989d23fa9
Erik Hedblom <Hedblom.e@gmail.com>
2017-01-23 20:44:02 +0100

    Added code for finding all excelfiles on a webpage and for searching an excelfile for a string.

---
+++ xcearch/htmlparser.py
@@ -0,0 +1,38 @@
+from urllib import request
+from html.parser import HTMLParser
+from urllib.parse import urlparse
+from urllib.parse import urlunparse
+
+
+class HTMLLinkParser(HTMLParser):
+    link = ""
+    documents = []
+
+    def __init__(self, url):
+        super().__init__()
+        self.base = urlparse(url)
+        html = request.urlopen(url).read().decode('utf-8')
+        self.feed(html)
+
+    def handle_starttag(self, tag, attrs):
+        if tag == "a":
+            for name, value in attrs:
+                if name == "href" and value.endswith('.xlsx'):
+                    u = urlparse(value)
+                    if not u[0]:
+                        self.link = urlunparse((self.base[0], self.base[1], u[2], u[3], u[4], u[5]))
+                    else:
+                        self.link = urlunparse(u)
+
+    def handle_data(self, data):
+        if self.link:
+            self.documents.append((data, self.link))
+            self.link = ""
+
+
+url = 'http://genome.cshlp.org/content/suppl/2017/01/17/gr.212720.116.DC1.html'
+
+parser = HTMLLinkParser(url)
+
+for d in parser.documents:
+    print(d)
