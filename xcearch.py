b2e4f7588af32950d75b4ab5eca5e46d147d13d5
Erik Hedblom <Hedblom.e@gmail.com>
2017-02-11 19:01:20 +0100

    Refactored and added Http User-Agent header

--- xcearch.py
+++ xcearch.py
@@ -1,11 +1,38 @@
 import argparse
 import io
 import re
-from urllib import request
+from html.parser import HTMLParser
+from urllib.parse import urlparse, urlunparse
+from urllib.request import Request, urlopen
 
 from openpyxl import load_workbook
 
-from xcearch.htmlparser import HTMLLinkParser
+
+class HTMLLinkParser(HTMLParser):
+    link = ""
+    documents = []
+
+    def __init__(self, url):
+        super().__init__()
+        self.base = urlparse(url)
+        req = Request(url, headers={'User-Agent': 'Excearch/0.1'})
+        html = urlopen(req).read().decode('utf-8')
+        self.feed(html)
+
+    def handle_starttag(self, tag, attrs):
+        if tag == "a":
+            for name, value in attrs:
+                if name == "href" and value.endswith('.xlsx'):
+                    u = urlparse(value)
+                    if not u[0]:
+                        self.link = urlunparse((self.base[0], self.base[1], u[2], u[3], u[4], u[5]))
+                    else:
+                        self.link = urlunparse(u)
+
+    def handle_data(self, data):
+        if self.link:
+            self.documents.append((data, self.link))
+            self.link = ""
 
 
 def main():
@@ -25,7 +52,7 @@ def main():
     for s in sites:
         for url in s[1]:
             print(url)
-            f = io.BytesIO(request.urlopen(url[1]).read())
+            f = io.BytesIO(urlopen(url[1]).read())
             wb = load_workbook(filename=f, read_only=True)
             result = []
             for ws in wb:
